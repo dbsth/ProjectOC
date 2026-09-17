ac828e7edf4868a639a10bcffa908e5822a14863
dbsth <144165923+dbsth@users.noreply.github.com>
2026-09-17 15:21:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,10 @@ const characters = {
         description:
             "Owner Character.",
 
-        story:`오너캐는 오너캐인데 관련 스토리가 없다.
-        그냥 오너캐인데 그런 게 필요할까...`
+        story:
+            "오너캐는 오너캐인데 관련 스토리가 없다.\n" +
+            "오너캐 이름 어케 정하는데..."
+
     },
 
 
